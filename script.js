f75597ef692d3afefb2db67ba6a7c26482e74c63
Ben <Ben@Air-van-Ben.fritz.box>
2021-07-04 12:58:17 +0200

    Add animations for balloons and background, hard work pays off

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const bodyBackground = document.querySelector('body');
 
-setTimeout(() => {
-    for (let i = 0; i < 6; i++) {
-        const randomNum = Math.floor(Math.random() * Math.floor(3) + 1);
-        const cloud = document.createElement('div');
-        cloud.setAttribute('class', `cloud${randomNum}`);
-        const image = document.createElement('img');
-        image.setAttribute('src', `./images/cloud${randomNum}.png`);
-        cloud.appendChild(image);
-        cloud.style.left = `${Math.floor(Math.random() * Math.floor(80))}vw`;
-        cloud.style.top = `${Math.floor(Math.random() * Math.floor(-40)) + -40}vh`;
+for (let i = 1; i < 7; i++) {
+    const randomNum = Math.floor(Math.random() * Math.floor(3) + 1);
+    const cloud = document.createElement('div');
+    cloud.setAttribute('class', `cloud${i}`);
+    const image = document.createElement('img');
+    image.setAttribute('src', `./images/cloud${randomNum}.png`);
+    cloud.appendChild(image);
+    cloud.style.left = `${Math.floor(Math.random() * Math.floor(80))}vw`;
+    cloud.style.top = `${Math.floor(Math.random() * Math.floor(-40)) + -40}vh`;
 
-        bodyBackground.appendChild(cloud);
-    }
-}, 5000);
+    bodyBackground.appendChild(cloud);
+}
